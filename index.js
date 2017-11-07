02fe8cc8bb9774f609663999104c662c14721211
Victoria <vitalina27@gmail.com>
2017-11-07 14:46:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function getFirstSelector(selector){
 let sel=document.querySelectorAll(selector)[0];
   return sel;
 }
-function nestedTarget(){}
+function nestedTarget(){
+
+  let sel=document.querySelector("#nested > ..target");
+  return sel;
+}
 
 function increaseRankBy(n){}
 function deepestChild(){}
